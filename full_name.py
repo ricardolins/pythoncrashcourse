909c9472afcbc6ed91d1a8666d4dfd4670dd3e86
ricardo lins <ricardo.lins@oracle.com>
2024-02-15 08:58:02 -0300

    Strings

---
+++ full_name.py
@@ -0,0 +1,12 @@
+#Combine two  strings variables
+first_name = "Ricardo"
+last_name = "Lins"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+#Combine two  strings variables
+first_name = "ricardo"
+last_name = "lins"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}"
+print(message)
