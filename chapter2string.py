03dac44b585aa716a458bce9815f08cd590face2
ricardo lins <ricardo.lins@oracle.com>
2024-02-16 14:02:27 -0300

    teste

---
+++ chapter2string.py
@@ -0,0 +1,31 @@
+# 1- Personal Message: Use a variable to represent a person’s name, 
+#and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+friend = 'José'
+
+message = f"Hi {friend}, how was your day?"
+
+print(message)
+
+
+# 2 2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case
+
+name = "josé"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# 2.5 Find a quote from a famous person you admire. Print the quote and the name of its author. 
+#Your output should look something like the following, including the quotation marks:
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+#2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. 
+#Then compose your message and represent it with a new variable called message. Print your message.
+
+famous_person = "Mano Brown"
+
+messagefamous = f"{famous_person} Said, Faith in God that he is just "
+
+print(messagefamous)
+
+
