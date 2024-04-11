c0e84859c696e970918ddeecd8a79c659ccd5dd7
ricardo lins <ricardo.lins@oracle.com>
2024-04-11 12:22:03 -0300

    Apostrophe

---
+++ apostrophe.py
@@ -0,0 +1,7 @@
+message = "One of Python's Strengths is its diverse community."
+print(message)
+
+#Error due a apostrophe
+message = 'One of Python's Strengths is its diverse community.'
+print(message)
+
