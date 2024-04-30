211df2afe19779e9fd333daccf13250f55e1cbec
Ricardo Lins <ricardobarbosalins@hotmail.com>
2024-04-30 12:04:30 -0300

    Lists

---
+++ bicycles.py
@@ -0,0 +1,7 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# pull Out the first bicycle in the list
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])
