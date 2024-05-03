9a9efe41b73eb2ed2bea3df26def4aeead7efaad
Ricardo Lins <ricardobarbosalins@hotmail.com>
2024-05-03 09:25:08 -0300

    Lists

--- bicycles.py
+++ bicycles.py
@@ -1,3 +1,6 @@
+
+#Working with lists
+
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 
@@ -5,3 +8,24 @@ print(bicycles)
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[0])
+
+
+# Add string methods to print a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+
+#print 2 items
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1].title())
+print(bicycles[3].title())
+
+#Returning the last element in a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
+
+#put a member od a list in a string and print a string
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {bicycles[0].title()}."
+
+print(message)
+
