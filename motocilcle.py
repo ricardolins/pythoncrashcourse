eee64cd0ddd27f6aea530f3c0403b061dacd1a3e
Ricardo Lins <ricardobarbosalins@hotmail.com>
2024-05-06 09:29:32 -0300

    List Exercises

---
+++ motocilcle.py
@@ -0,0 +1,11 @@
+# Changing an element in a list
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+#Appeding or Adding a new item to the end of the list.
+motorcycles.append("Kasynsky")
+print(motorcycles)
