73d5649ebc9476040e136af4375bcd90bdf1182a
Ricardo Lins <ricardobarbosalins@hotmail.com>
2024-05-07 09:44:05 -0300

    Working With Lists

--- motocilcle.py
+++ motocilcle.py
@@ -9,3 +9,26 @@ print(motorcycles)
 #Appeding or Adding a new item to the end of the list.
 motorcycles.append("Kasynsky")
 print(motorcycles)
+
+#names Family with append
+family = []
+
+family.append('Thalita')
+family.append('Angelina')
+family.append('Ricardo')
+family.append("Nicolas")
+family.append("Maria")
+
+print(family)
+
+#Insert an item in a list
+
+family.insert(1, "Antonio")
+family.insert(2, "Antonio")
+
+print(family)
+
+#Remove an Item in a List
+
+del family[2]
+print(family)
